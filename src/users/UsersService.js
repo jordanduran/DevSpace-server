36d946a9cf0350e0cdcaaa7a510cc5a8f3acff31
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-11-03 21:15:54 -0500

    UserService operations

--- src/users/UsersService.js
+++ src/users/UsersService.js
@@ -1,7 +1,34 @@
 const UsersService = {
     getAllUsers(db) {
         return db('users')
-    }
-}
+    },
+    createUser(db, newUser) {
+        return db
+            .insert(newUser)
+            .into('users')
+            .returning('*')
+            .then(rows => rows[0]);
+    },
+    deleteUser(db, id) {
+        return db('users')
+            .where({ id })
+            .delete();
+    },
+    updateUser(db, id, newUser) {
+        return db('users')
+            .where({ id })
+            .update(newUser)
+            .first();
+    },
+    getUsersById(db, id){
+        return db
+            .select('*')
+            .from('users')
+            .where({ id })
+            .first();
+    },
+};
+
+module.exports = noteService;
 
 module.exports = UsersService;
