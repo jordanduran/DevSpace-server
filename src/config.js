1c168c6dd4f927d1a0730d9a4c3f1a574a1a9f60
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-11-09 10:57:54 -0500

    fixed db url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/devspace'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://jvubwqyirgrtul:e64094a9d75269a252f99fe606af7ef783fd45cc90438aaf6bc06dfd58d42597@ec2-54-235-180-123.compute-1.amazonaws.com:5432/dcg5e046an4ava'
   }
